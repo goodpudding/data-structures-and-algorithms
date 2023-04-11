1c7d4ea47b7679b63083f8ce7102801a0d978f21
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-04-11 14:44:40 -0700

    Done with lab 6

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,79 +1,132 @@
-"use strict";
+'use strict';
 
 // Require our linked list implementation
-const LinkedList = require("../index");
+const LinkedList = require('../index');
 
-describe("Testing the Linked List data structure", () => {
-  test("Can successfully instantiate an empty linked list", () => {
+describe('Testing the Linked List data structure', () => {
+  test('Can successfully instantiate an empty linked list', () => {
     let newList = new LinkedList();
     expect(newList).toBeTruthy();
   });
 
-  test("Can properly insert into the linked list", () => {
+  test('Can properly insert into the linked list', () => {
     let newList = new LinkedList();
-    newList.insert("Tacos");
-    expect(newList.traverse().value).toEqual("Tacos");
+    newList.insert('Tacos');
+    expect(newList.traverse().value).toEqual('Tacos');
   });
 
-  test("The head property will properly point to the first node in the linked list", () => {
+  test('The head property will properly point to the first node in the linked list', () => {
     let newList = new LinkedList();
-    newList.insert("Tacos");
-    newList.append("Cheese");
-    newList.append("Pizza");
-    expect(newList.head.value).toEqual("Tacos");
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    expect(newList.head.value).toEqual('Tacos');
   });
 
-  test("Can properly insert multiple nodes into the linked list", () => {
+  test('Can properly insert multiple nodes into the linked list', () => {
     let newList = new LinkedList();
-    newList.insert("Tacos");
-    newList.append("Cheese");
-    newList.append("Pizza");
-    expect(newList.includes("Tacos")).toEqual(true);
-    expect(newList.includes("Cheese")).toEqual(true);
-    expect(newList.includes("Pizza")).toEqual(true);
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    expect(newList.includes('Tacos')).toEqual(true);
+    expect(newList.includes('Cheese')).toEqual(true);
+    expect(newList.includes('Pizza')).toEqual(true);
   });
 
-  test("Will return true when finding a value within the linked list that exists", () => {
+  test('Will return true when finding a value within the linked list that exists', () => {
     let newList = new LinkedList();
-    newList.insert("Tacos");
-    newList.append("Cheese");
-    newList.append("Pizza");
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
     let isTrue = false;
-    if (newList.includes("Cheese")) {
+    if (newList.includes('Cheese')) {
       isTrue = true;
     }
     expect(isTrue).toEqual(true);
-    expect(newList.includes("Tacos")).toEqual(true);
-    expect(newList.includes("Cheese")).toEqual(true);
-    expect(newList.includes("Pizza")).toEqual(true);
+    expect(newList.includes('Tacos')).toEqual(true);
+    expect(newList.includes('Cheese')).toEqual(true);
+    expect(newList.includes('Pizza')).toEqual(true);
   });
 
-  test("Will return false when searching for a value in the linked list that does not exist", () => {
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
     let newList = new LinkedList();
-    newList.insert("Tacos");
-    newList.insert("Cheese");
-    newList.insert("Pizza");
-    expect(newList.includes("Salad")).toEqual(false);
+    newList.insert('Tacos');
+    newList.insert('Cheese');
+    newList.insert('Pizza');
+    expect(newList.includes('Salad')).toEqual(false);
   });
 
-  test("Can properly return a collection of all the values that exist in the linked list", () => {
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
     let newList = new LinkedList();
-    newList.insert("Tacos");
-    newList.append("Cheese");
-    newList.append("Pizza");
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
     expect(newList.toString(newList)).toEqual(
-      "{ Tacos } -> { Cheese } -> { Pizza } -> NULL"
+      '{ Tacos } -> { Cheese } -> { Pizza } -> NULL'
     );
   });
 });
 
-describe("Tesing for lab 6", () => {
-  test("Can successfully add a node to the end of the linked list", () => {
-    
+describe('Tesing for lab 6', () => {
+  test('Can successfully add a node to the end of the linked list', () => {
+    let newList = new LinkedList();
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    expect(newList.toString(newList)).toEqual(
+      '{ Tacos } -> { Cheese } -> { Pizza } -> NULL'
+    );
+
   });
-  test("Can successfully add multiple nodes to the end of a linked list", () => {});
-  test("Can successfully insert a node before a node located i the middle of a linked list", () => {});
-  test("Can successfully insert a node before the first node of a linked list", () => {});
-  test("Can successfully insert after a node in the middle of the linked list", () => {});
-  test("Can successfully insert a node after the last node of the linked list", () => {});
+  test('Can successfully add multiple nodes to the end of a linked list', () => {
+    let newList = new LinkedList();
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    expect(newList.toString(newList)).toEqual(
+      '{ Tacos } -> { Cheese } -> { Pizza } -> NULL'
+    );
+  });
+  test('Can successfully insert a node before a node located in the middle of a linked list', () => {
+    let newList = new LinkedList();
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    newList.insertBefore('Cheese', 'Burrito');
+    expect(newList.toString(newList)).toEqual(
+      '{ Tacos } -> { Burrito } -> { Cheese } -> { Pizza } -> NULL'
+    );
+  });
+  test('Can successfully insert a node before the first node of a linked list', () => {
+    let newList = new LinkedList();
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    newList.insertBefore('Tacos', 'Burrito');
+    expect(newList.toString(newList)).toEqual(
+      '{ Burrito } -> { Tacos } -> { Cheese } -> { Pizza } -> NULL'
+    );
+  });
+  test('Can successfully insert after a node in the middle of the linked list', () => {
+    let newList = new LinkedList();
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    newList.insertAfter('Cheese', 'Burrito');
+    expect(newList.toString(newList)).toEqual(
+      '{ Tacos } -> { Cheese } -> { Burrito } -> { Pizza } -> NULL'
+    );
+
+  });
+  test('Can successfully insert a node after the last node of the linked list', () => {
+    let newList = new LinkedList();
+    newList.insert('Tacos');
+    newList.append('Cheese');
+    newList.append('Pizza');
+    newList.append('Burrito');
+    expect(newList.toString(newList)).toEqual(
+      '{ Tacos } -> { Cheese } -> { Pizza } -> { Burrito } -> NULL'
+    );
+
+  });
 });
